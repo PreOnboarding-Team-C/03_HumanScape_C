38974fd14ac5840b6578f89f62c4eea6fb7c2837
Ashbee Kim <ksb.forest@gmail.com>
2022-05-10 20:01:02 +0900

    fix : conflict 해결을 위한 수정

--- utils/time_stamp.py
+++ utils/time_stamp.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-from django.db.models import Model, DateTimeField
-
-
-class TimeStampModel(Model):
-    created_datetime = DateTimeField(auto_now_add=True)
-    
-=======
 from django.db import models
 
 class TimeStampModel(models.Model):
     created_datetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
 
->>>>>>> 0bb680528e5db086dd9e37128e9e35d4e436d0ce
+
     class Meta:
         abstract = True
