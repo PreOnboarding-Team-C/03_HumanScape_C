34f9df431e0c15919862b3866930fd706d663e04
do-not-do-that <rl7968@naver.com>
2022-05-12 15:17:25 +0900

    feat : #9 data-insert update/create 기능 구현

--- utils/uploader.py
+++ utils/uploader.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from apps.projects.models import Project
 
 def insert_data(data):
-    data_set = []
+    col = ['number', 'title', 'research_period', 'research_scope', 'research_case', \
+        'research_responsible_institution', 'research_phase', 'total_subject_count',\
+        'speciality']
 
     for rows in data:
         insert_period = 0
@@ -20,21 +23,28 @@ def insert_data(data):
         # 전체목표연구대상자 수 int 형변환
         if rows['전체목표연구대상자수']:
             insert_subject_count = int(rows['전체목표연구대상자수'])
+        
+        project = {
+            'number': rows['과제번호'],
+            'title': rows['과제명'],
+            'research_period': insert_period,
+            'research_scope': rows['연구범위'],
+            'research_case': rows['연구종류'],
+            'research_responsible_institution': rows['연구책임기관'],
+            'research_phase': rows['임상시험단계(연구모형)'],
+            'total_subject_count': insert_subject_count,
+            'speciality': rows['진료과']
+        }
+        
+        _project = Project.objects.filter(number=rows['과제번호']).order_by('number').distinct().values(*col).first()
 
-        data_set.append(
-            Project(
-                number = rows['과제번호'],
-                title = rows['과제명'],
-                research_period = insert_period,
-                research_scope = rows['연구범위'],
-                research_case = rows['연구종류'],
-                research_responsible_institution = rows['연구책임기관'],
-                research_phase = rows['임상시험단계(연구모형)'],
-                total_subject_count = insert_subject_count,
-                speciality = rows['진료과'],
-            )
-        )
+        if not _project: # 과제번호가 기존 데이터에 없는 번호라면 데이터 생성 
+            Project.objects.create(**project)
+        else:
+            if project != _project: # 변경된 데이터라면 update
+                del project['number']
+                Project.objects.filter(number=rows['과제번호']).update(**project, updated_datetime=datetime.now())
 
-    Project.objects.bulk_create(data_set)
     print(f"{'='*25} Project DATA UPLOADED SUCCESSFULLY {'='*25}")
+
 
