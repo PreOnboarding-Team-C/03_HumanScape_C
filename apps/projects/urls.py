0a760860a1743f4e65420f6785be1b0b58bb5544
Bruno-Jang <sosdnrud@gmail.com>
2022-05-12 16:40:21 +0900

    feat : #14 offset & limit 적용

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -6,6 +6,6 @@ from .views import ProjectListAPIView, ProjectDetailAPIView, CheckUpdatedDataAPI
 urlpatterns = [
     # path('/test', TestAPIView.as_view()),
     path('', ProjectListAPIView.as_view()),
+    path('/list', CheckUpdatedDataAPIView.as_view()),   # 참고사항 : '/' + string 형태의 url 경우에는 아래의 '/<str:number>' url 위에 작성해야 404 에러가 발생하지 않습니다.
     path('/<str:number>', ProjectDetailAPIView.as_view()),
-    path('/list', CheckUpdatedDataAPIView.as_view()),
 ]
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models import F
+from django.db.models import F, Q
 
 from .models import Project
 from apps.projects.models import Project
@@ -37,10 +37,13 @@ class CheckUpdatedDataAPIView(APIView):
     '''
     # days에 원하는 날짜 입력 => 최근 입력한 날짜 내의 업데이트 된 임상정보 리스트 리턴
     def get(self, request):
+        offset = int(request.GET.get('offset', 0))
+        limit = int(request.GET.get('limit', 10))
+        
         days = 7 
         diff_datetime = datetime.datetime.now() - datetime.timedelta(days=days)
 
-        projects_queryset = Project.objects.filter(created_datetime=F('updated_datetime'), updated_datetime__gte=diff_datetime)        
+        projects_queryset = Project.objects.filter(~Q(created_datetime=F('updated_datetime')), updated_datetime__gte=diff_datetime).order_by('number')[offset : offset+limit]
         serializer = ProjectSerializer(projects_queryset, many=True)
         return Response(serializer.data, status=200)
 
